63d71b15abd972d1c2c3c3d761a58bd75108dad1
Tejes Mote <tejesmote148@gmail.com>
2025-08-27 00:38:50 +0530

    Initial DB and Migration Implementation

--- backend/UserService/UserService.Api/Program.cs
+++ backend/UserService/UserService.Api/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// DbContext
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ backend/UserService/UserService.Core/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }   // store hash, not plain text
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
